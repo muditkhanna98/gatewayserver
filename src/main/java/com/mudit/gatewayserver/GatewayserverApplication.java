e647b52a9e095c0da6ae532f89b927216b557424
Mudit Khanna <muditkhanna998@gmail.com>
2026-02-15 17:03:00 -0500

    Configure Gateway Server routing and update Docker Compose
    - Define custom routes for `accounts`, `loans`, and `cards` in `GatewayserverApplication`
    - Update `gatewayserver` `application.yaml` to integrate with Eureka, Config Server, and expose endpoints
    - Add `jib-maven-plugin` to `gatewayserver/pom.xml` for containerization
    - Comment out application services in `docker-compose.yml`, retaining only databases

--- src/main/java/com/mudit/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/mudit/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.mudit.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,28 @@ public class GatewayserverApplication {
         SpringApplication.run(GatewayserverApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator bankRouteConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(predicateSpec -> predicateSpec
+                        .path("/bank/accounts/**")
+                        .filters(f ->
+                                f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS"))
+                .route(predicateSpec -> predicateSpec
+                        .path("/bank/loans/**")
+                        .filters(f ->
+                                f.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(predicateSpec -> predicateSpec
+                        .path("/bank/cards/**")
+                        .filters(f ->
+                                f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS")).build();
+
+
+    }
+
 }
+
+
