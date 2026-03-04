907a237d94ed23ca8fe37f5a8f7d7c433845e455
Mudit Khanna <muditkhanna998@gmail.com>
2026-03-03 23:11:24 -0500

    Add circuit breaker support and fallback mechanism in Gateway Server

--- src/main/java/com/mudit/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/mudit/gatewayserver/GatewayserverApplication.java
@@ -22,7 +22,10 @@ public class GatewayserverApplication {
                         .path("/bank/accounts/**")
                         .filters(f ->
                                 f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}")
-                                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                        .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+                                                .setFallbackUri("forward:/contactSupport"))
+                        )
                         .uri("lb://ACCOUNTS"))
                 .route(predicateSpec -> predicateSpec
                         .path("/bank/loans/**")
